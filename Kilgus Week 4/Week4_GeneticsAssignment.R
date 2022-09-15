2456c7e067d0520eca06e7b3caab2bbaf5bb4855
Maris Kilgus <112492290+mariskilgus@users.noreply.github.com>
2022-09-15 08:45:36 -0400

    Download
    Just downloaded

---
+++ Kilgus Week 4/Week4_GeneticsAssignment.R
@@ -0,0 +1,17 @@
+# What happens to the maximum parsimony calculation when you move a node in the unrooted tree from this week's exercise? (4 points)
+  # Use the term "similarity matrix" in your answer.
+  # Hint: Why does it take longer to calculate than the original tree, and even more so if you move the point further?
+
+
+# For your scripting assignment we will use the "ge_data" data frame found in the "stability" package.
+  # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". 
+
+# Create two linear models for Yield Response: one related to the Environment and one to the Genotype.
+  # 'Yield Response' in this dataset is a measure of phenotype expression.
+  # Hint: Look at the help file for this dataset.
+
+# Test the significance of both models and look at the model summary.
+  # Which model is a better fit to explain the yield response, and WHY? (4 points)
+    # Hint: Does one model seem more likely to be over-fitted?
+
+# Which environment would be your very WORST choice for generating a strong yeild response? (2 points)
