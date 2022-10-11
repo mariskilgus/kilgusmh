f1f0bd6047ae12d6186a08a823500dd8b0a982a0
Maris Kilgus <112492290+mariskilgus@users.noreply.github.com>
2022-10-10 23:37:33 -0400

    I need help
    I got 3 errors and I tried something different for each

--- Week6/Week6_MixedModel_Assignment.R
+++ Week6/Week6_MixedModel_Assignment.R
@@ -8,6 +8,39 @@
     # In the other model include one interactive effect.
     # Use a binomial distribution and block as a random effect in both models to match the paper's analyses. Remember ?family to find distribution names.
 
+
+setwd("C:/Users/Kilgus/Documents/GitHub/kilgusmh/Week6")
+
+df <- read.csv("Toscano_Griffen_Data.csv")
+install.packages("MASS")
+library(MASS)
+install.packages("MuMIn")
+library(MuMIn)
+
+glmm.mod <- glmmPQL(Flight.initiation.distance..FID.~Object, family = gaussian, random = ~ 1 | ID, data = df)
+summary(glmm.mod)
+
+r.squaredGLMM(glmm.mod)
+glmm.mod2 <- glmmPQL(Flight.initiation.distance..FID.~Object + Area, family = gaussian, random = ~ 1 | ID, data = df)
+summary(glmm.mod2)
+r.squaredGLMM(glmm.mod2)
+plot(glmm.mod2)
+hist(df$Flight.initiation.distance..FID.)
+glmm.mod3 <- glmmPQL(Flight.initiation.distance..FID.~Object + Area, family = Gamma, random = ~ 1 | ID, data = df)
+plot(glmm.mod3)
+summary(glmm.mod3)
+r.squaredGLMM(glmm.mod3)
+install.packages("mgcv")
+library(mgcv)
+gam.mod1 <- gam(Flight.initiation.distance..FID.~Object + Area, family = Gamma, random = list(ID=~ 1), data = df)
+summary(gam.mod1)
+plot(gam.mod1$residuals, ylim = c(-.1,.1))
+vis.gam(gam.mod1, view=c("Object","Area"), theta = 45, color = "heat")
+AIC(gam.mod1) 
+gam.mod2 <- gam(Flight.initiation.distance..FID.~ Object*Area, family = Gamma, random = list(ID=~ 1), data = df)
+summary(gam.mod2)
+plot(gam.mod2$residuals, ylim = c(-.1,.1))
+
 # The authors used proportional consumption of prey as the (y) in their model, but did not include this in the dataset.
   # So we are going to create it - run the following line, assuming df= your data frame (feel free to change that):
 df$prop.cons <- df$eaten/df$prey 
@@ -22,6 +55,7 @@ df$prop.cons <- df$eaten/df$prey
 
 
 # Re-run both models as generalized additive models instead (using gam). Then compare the AIC of both models. (4 points each)
+AIC(gam.mod1, gam.mod2)
 
 
 # (Q4) - Which model is a better fit? (2 pt)
