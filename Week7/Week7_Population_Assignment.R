fa4c085086048e9a89da6ea1b502ff089cd148a6
Maris Kilgus <112492290+mariskilgus@users.noreply.github.com>
2022-10-20 09:53:40 -0400

    need help
    so many errors

--- Week7/Week7_Population_Assignment.R
+++ Week7/Week7_Population_Assignment.R
@@ -10,24 +10,28 @@ data <- read.csv("Plankton_move_average.csv")
 # These are data from the Great Lakes Environmental Research Laboratory plankton sampling.
 
 #Used the following lines to format the date and remove NAs from the dataset:
-data$Date <- as.Date(data$Date, origin = "0001-01-01") # Setting values to "day zero".
+data$Date <- as.Date(data$Date, origin = "0001-01-01") 
+# Setting values to "day zero".
 data <- na.omit(data)
 
 #Plot these population data over time with the following code:
 plot(data)
+
 ggplot(data)
-xlab("Numeric Date") + ylab("Density Individuals")
+
+xlab("NumericDate") + ylab("DensityIndividuals")
+
 geom_line(data=data, aes(Date, D.mendotae), color="black", alpha = 0.7, size=1)
 geom_line(data=data, aes(Date, LimncalanusF+LimncalanusM), color="orange",  alpha = 0.7, size=1)
 # adding males and females together, hint: this is actually spelled Limnocalanus
 geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)
 geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)
+
 theme_bw() 
 
 # Export this plot to have on hand for reference in the next section of the assignment (and upload with your script).
 df
 # (1) - Which species is most likely to be r-selected prey and which its primary predator? (2 pts)
-
 #
 # What is one relationship the third species MIGHT have to the first two? (2 pts)
 #
@@ -36,17 +40,22 @@ df
 install.packages("deSolve")
 library(deSolve)
 dev.off()
-LotVmod <- function (Time, State, Pars) {with(as.list(c(State, Pars)), {dx = x*(alpha - beta*y)dy = -y*(gamma - delta*x)return(list(c(dx, dy)))
+LotVmod <- function (Time, State, Pars) '{with(as.list(c(State, Pars)), {dx = x*(alpha - beta*y)dy = -y*(gamma - delta*x)return(list(c(dx, dy)))'
 
 
 # (2) - What do alpha, beta, gamma, and delta represent in this function? (4 pts)
 #They are each of the variables for predator and prey.
 
 # (3) - By only changing values for alpha, beta, gamma, and/or delta
-Pars <- c(alpha = 2, beta = 0.5, gamma = .2, delta = .6) #This is the line we will change
-State <- c(x = 10, y = 10)#For now keep this the same.
-Time <- seq(0, 100, by = 1)#For now keep this the same.
+Pars <- c(alpha = 2, beta = 0.5, gamma = .2, delta = .6) 
+#This is the line we will change
+State <- c(x = 10, y = 10)
+#For now keep this the same.
+Time <- seq(0, 100, by = 1)
+#For now keep this the same.
+
 out <- as.data.frame(ode(func = LotVmod, y = State, parms = Pars, times = Time)) 
+
 matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
 legend("topright", c("Rabid foxes", "Cute bunnies"), lty = c(1,2), col = c(1,2), box.lwd = 0)
 
