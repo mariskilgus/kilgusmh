051e89f9a27dcce64c39a61bb47822afed822e8d
Maris Kilgus <112492290+mariskilgus@users.noreply.github.com>
2022-11-30 15:24:58 -0500

    outline
    out

---
+++ FinalProject/Final.R
@@ -0,0 +1,5 @@
+#Dryad link https://datadryad.org/stash/dataset/doi:10.5061%2Fdryad.t7f3rc8
+#Question: How do antibiotics effect greenhouse emissions from cows?
+#Data: Cows that were treated with benzylpenicillin and tetracycline. Total Greenhouse emissions before and after antibiotic treatments.
+#Figures: Box plot of Greenhouse emissions before, Boxplot of Greenhouse emissions after, and bar graph of averages of before treatment and after treatment
+#Data processing: Exclude milk production, individual gasses emission,
