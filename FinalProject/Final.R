54839906098d62945d4da3b001d284b3f91e0163
Maris Kilgus <112492290+mariskilgus@users.noreply.github.com>
2022-12-07 17:05:13 -0500

    save
    finally

--- FinalProject/Final.R
+++ FinalProject/Final.R
@@ -12,49 +12,21 @@ install.packages("vegan")
 library(vegan)
 
 beetlegas.csv <- read.csv("beetlegas.csv", header=T)
-cowgas.xlsx <- read.csv("cowgas.xlsx", header=T)
-Cowgas <- read_excel(system.file("extdata",
-                                        "cowgas.xlsx",
-                                               package = "datplot",
-                                               mustWork = TRUE))
+cowgas.csv <- read.csv("cowgas.csv", header=T)
 
 
+data <- rbind(beetlegas.csv,cowgas.csv)
 
-# accessing all the sheets
-sheet = excel_sheets("cowgas.xlsx")
 
-# applying sheet names to dataframe names
-data_frame = lapply(beetlegas.csv, cowgas.xlsx(beetlegas.csv, cowgas.xlsx),
-                    function(x) read_excel("beetlegas.csv", "sheet=x"cowgas.xlsx))
+mergedf <- merge(beetlegas.csv,cowgas.csv,bF "row", All=T) 
 
-# attaching all dataframes together
-data_frame = bind_rows(data_frame, .id="Sheet")
 
-# printing data of all sheets
-print (data_frame)
 
 
 
 
 
 plot(beetlegas.csv)
-plot(cowgas.xlsx)
+plot(cowgas.csv)
 
 
-
-
-ozone <- airquality$Ozone
-temp <- airquality$Temp
-
-ozone_norm <- rnorm(200,mean=mean(ozone, na.rm=TRUE), sd=sd(ozone, na.rm=TRUE))
-temp_norm <- rnorm(200,mean=mean(temp, na.rm=TRUE), sd=sd(temp, na.rm=TRUE))
-boxplot(ozone, ozone_norm, temp, temp_norm,
-        main = "Multiple boxplots for comparision",
-        at = c(1,2,4,5),
-        names = c("antibiotic1", "antibiotic2", "control1", "control2"),
-        las = 2,
-        col = c("green","red"),
-        border = "blue",
-        horizontal = TRUE,
-        notch = TRUE
-)
